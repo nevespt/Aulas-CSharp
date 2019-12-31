84e90b7935e49d09203bd3595acea7be61dd0d86
NevesPT <50218490+nevespt@users.noreply.github.com>
2019-12-31 12:06:05 +0000

    removed empty or not finished project

--- C-Sharp-Proxy-Server-master/proxyServer/bin/Debug/gencert.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd "F:\Visual Studio 2017\Projects\proxyServer\proxyServer\bin\Debug"
-F:
-makecert.exe certs\general.xcer -a sha256 -n "CN = example.com" -sky signature -pe -len 2048
-echo Operation Completed!
